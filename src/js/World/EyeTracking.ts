af35d9e5fbfd9b259b5461636eddac2d03c53bb2
romanechouteau <romane.chouteau@gmail.com>
2021-03-16 13:39:13 +0100

    feat(eyetracking): add face

--- src/js/World/EyeTracking.ts
+++ src/js/World/EyeTracking.ts
@@ -1,13 +1,18 @@
-import { BoxGeometry, Mesh, MeshNormalMaterial, Object3D, Raycaster, SphereGeometry, Vector2 } from 'three'
-import { reduce, map, size } from 'lodash'
+import { BufferGeometry, Mesh, MeshNormalMaterial, Object3D, Raycaster, SphereGeometry, Vector2, BufferAttribute, Points, PointsMaterial, DoubleSide } from 'three'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { reduce, map, size, flatMap } from 'lodash'
 
 // @ts-ignore
 import Mouse from '@tools/Mouse'
 // @ts-ignore
+import faceSrc from '@models/face.glb'
+// @ts-ignore
 import webgazer from '@tools/WebGazer'
 
+const loader = new GLTFLoader()
+
 export default class EyeTracking {
-    face: Mesh
+    face: Points
     mouse: Mouse
     camera: any
     points: Mesh[]
@@ -18,6 +23,8 @@ export default class EyeTracking {
     windowWidth: number
     windowHeight: number
     pointsClicks: {}
+    faceGeometry: BufferGeometry
+    facePositions: number[]
 
     constructor(options: { windowWidth: number, windowHeight: number, mouse: Mouse, camera: any }) {
         const { windowWidth, windowHeight, mouse, camera } = options
@@ -34,18 +41,12 @@ export default class EyeTracking {
         this.raycaster = new Raycaster()
         this.calibrated = false
         this.pointsClicks = {}
+        this.facePositions = []
 
         this.setWebGazer()
         this.setFace()
         this.setCalibrationPoints()
         this.listenMouseDown()
-    }
-
-    setFace() {
-        const geometry = new BoxGeometry(1, 1, 1)
-        const material = new MeshNormalMaterial()
-        this.face = new Mesh(geometry, material)
-        this.container.add(this.face)
     }
 
     setCalibrationPoints() {
@@ -82,14 +83,62 @@ export default class EyeTracking {
         webgazer.showPredictionPoints(false)
     }
 
+    async setFace() {
+        // VISAGE PERSONNALISE
+        this.faceGeometry = new BufferGeometry()
+
+        // VISAGE MODEL
+        // const faceModel = (await loader.loadAsync(faceSrc)).scene
+        // // @ts-ignore
+        // this.faceGeometry = faceModel.children[2].geometry
+        // this.faceGeometry.rotateX(Math.PI / 2)
+
+        const material = new PointsMaterial({
+            size: 0.04,
+            sizeAttenuation: true,
+            color: 0xff0000,
+            side: DoubleSide
+        })
+        this.face = new Points(this.faceGeometry, material)
+
+        // VISAGE PERSONNALISE
+        this.face.scale.set(-0.01, -0.01, -0.01)
+
+        // VISAGE MODEL
+        // this.face.scale.set(10, 10, 10)
+
+        this.container.add(this.face)
+    }
+
     getPredictions(data) {
         if (data === null) {
             return
         }
 
+        // VISAGE PERSONNALISE SUIT REGARD
+        if (this.facePositions.length === 0) {
+            // VISAGE PERSONNALISE SUIT MOUVEMENTS
+            this.facePositions = flatMap(webgazer.getTracker().positionsArray, position =>
+                [
+                    position[0] - 310,
+                    position[1] - 250,
+                    position[2]
+                ]
+            )
+            const positionsArray = new Float32Array(this.facePositions)
+
+            this.faceGeometry.setAttribute(
+                'position',
+                new BufferAttribute(positionsArray, 3)
+            )
+        // VISAGE PERSONNALISE SUIT REGARD
+        }
+
         // valeur 4 arbitraire pour le moment
         const x = (data.x / this.windowWidth - 0.5) * 4
         const y = - (data.y / this.windowHeight - 0.5) * 4
+
+        // VISAGE SUIT REGARD
         this.face.lookAt(x, y, 5)
 
         if (this.calibrated) {
@@ -101,10 +150,10 @@ export default class EyeTracking {
         // valeur radius arbitraire pour le moment
         // valeur duration à varier selon la durée du regard au centre attendue
         const radius = 1
-        const duration = 100
+        const duration = 50
         const isInZone = Math.pow(x, 2) + (Math.pow(y, 2)) < Math.pow(radius, 2)
 
-        if (this.inZone.length < 100) {
+        if (this.inZone.length < duration) {
             return this.inZone.push(isInZone)
         }
 
