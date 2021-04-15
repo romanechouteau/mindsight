50119b0c3935eff2af5cc655c569d1cc2b51e19d
romanechouteau <romane.chouteau@gmail.com>
2021-04-15 19:02:34 +0200

    :sparkles: new ground model

--- src/js/World/Ground.ts
+++ src/js/World/Ground.ts
@@ -1,7 +1,7 @@
 import { Object3D, Mesh, MeshBasicMaterial, DoubleSide, Group, BoxBufferGeometry, MeshNormalMaterial, Vector3 } from 'three'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 // @ts-ignore
-import reliefSrc from '@models/angy_environment.glb'
+import reliefSrc from '@models/mapTest.glb'
 import groundSrc from '@models/sol__.glb'
 
 const loader = new GLTFLoader()
@@ -29,13 +29,17 @@ export default class Ground {
   async createGround() {
     this.relief = (await loader.loadAsync(reliefSrc)).scene
     this.ground = (await loader.loadAsync(groundSrc)).scene
+    this.ground.visible = false
+    this.relief.scale.set(0.01, 0.01, 0.01)
+    this.relief.rotation.y = Math.PI
+    this.relief.position.z = 10
     // this.fakeGround = this.ground.clone(true)
     // this.fakeGround.children.forEach(mesh => (mesh as Mesh).material = new MeshBasicMaterial({ opacity: 0, side: DoubleSide }))
     // this.container.add(this.fakeGround)
     this.container.add(this.relief)
     this.container.add(this.ground)
-    
-    this.relief.getObjectByName('Sol').visible = false
+
+    // this.relief.getObjectByName('Sol').visible = false
     // geometryModifier.subdividePlane((this.relief.getObjectByName('Sol') as Mesh), 1024)
   }
 
