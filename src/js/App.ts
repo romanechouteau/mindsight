706650c0d30d853c9ef9448e33153bb058ac4108
romanechouteau <romane.chouteau@gmail.com>
2021-04-15 16:18:28 +0200

    :sparkles: new sky color

--- src/js/App.ts
+++ src/js/App.ts
@@ -67,7 +67,7 @@ export default class App {
     this.renderer.outputEncoding = sRGBEncoding
     this.renderer.gammaFactor = 2.2
     // Set background color
-    this.renderer.setClearColor(0x212121, 1)
+    this.renderer.setClearColor(0xF4C5B5, 1)
     // Set renderer pixel ratio & sizes
     this.renderer.setPixelRatio(window.devicePixelRatio)
     this.renderer.setSize(this.sizes.viewport.width, this.sizes.viewport.height)
--- src/js/World/index.ts
+++ src/js/World/index.ts
@@ -104,7 +104,7 @@ export default class World extends Component {
     this.container.add(this.ground.container)
   }
   setFog() {
-    const fog = new FogExp2(0x212121, 0.08)
+    const fog = new FogExp2(0xF4C5B5, 0.08)
     // const fog = new Fog(0x212121, 0, 10)
     App.scene.fog = fog
   }
