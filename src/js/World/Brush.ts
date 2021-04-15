c9b6fd17f5c9f1cc474c4e8bcf2e36a68eef62c1
romanechouteau <romane.chouteau@gmail.com>
2021-04-15 10:32:37 +0200

    :lipstick: remove toggle and labels

--- src/js/World/Brush.ts
+++ src/js/World/Brush.ts
@@ -258,8 +258,7 @@ export default class Brush extends Component {
       event.preventDefault()
       const key = event.key || event.keyCode
       if (isEqual(key, ' ') || isEqual(key, 'Space') || isEqual(key, 32)) {
-        const checkbox = <HTMLInputElement> this.element.querySelector('#canDraw')
-        store.dispatch('updateBrushParams', { param: 'canDraw', value: !checkbox.checked })
+        store.dispatch('updateBrushParams', { param: 'canDraw', value: !store.state.brush.canDraw })
       }
     }
     document.addEventListener('keyup', this.keyboardListener)
@@ -272,7 +271,6 @@ export default class Brush extends Component {
     this.element.innerHTML = `
       <div class="paramsGroup bigAndSmallCircles">
         <div class="inputGroup circleRangeGroup">
-          <div class="label">Brush size</div>
           <div
               class="circleRange"
               id="size"
@@ -301,19 +299,6 @@ export default class Brush extends Component {
         </div>
       </div>
       <div class="paramsGroup">
-        <div class="inputGroup checkbox">
-          <div class="checkboxWrapper">
-            <div class="toggle">
-              <input
-                type="checkbox"
-                name="canDraw"
-                id="canDraw"
-                ${store.state.brush.canDraw && 'checked'} />
-              <div class="slider"></div>
-            </div>
-          </div>
-          <label for="canDraw">Draw</label>
-        </div>
         <div class="inputGroup">
         <input
         type="range"
@@ -323,7 +308,6 @@ export default class Brush extends Component {
         name="particleSize"
         id="particleSize"
         value="${store.state.brush.particleSize}" />
-        <label for="particleSize">Particle size</label>
         </div>
         <div class="inputGroup">
           <input
@@ -334,7 +318,6 @@ export default class Brush extends Component {
             name="count"
             id="count"
             value="${store.state.brush.count}" />
-          <label for="count">Density</label>
         </div>
       </div>
     `
@@ -408,11 +391,6 @@ export default class Brush extends Component {
 
         input.addEventListener('change', () => {
           const value = parseFloat(input.value)
-          store.dispatch('updateBrushParams', { param, value })
-        })
-      } else if (isEqual(type, 'checkbox')) {
-        input.addEventListener('change', () => {
-          const value = input.checked
           store.dispatch('updateBrushParams', { param, value })
         })
       }
