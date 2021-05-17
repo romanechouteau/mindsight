7412bf84ed0fb57ebc93017723054da0a6645220
AyymericArn <aymeric@clintagency.com>
2021-05-17 11:16:34 +0200

    feat(wb-cursor): add blur and width control

--- src/js/Behavior/WorldBuilder/index.ts
+++ src/js/Behavior/WorldBuilder/index.ts
@@ -78,9 +78,9 @@ export default class WorldBuilder extends Component {
         const canvas: HTMLCanvasElement = document.querySelector('#worldBuilder canvas')
         const ctx = canvas.getContext('2d')
         const {width, height} = canvas
-        const config = { steps: 200, opacity: 1, waveLength: 50, speed: 250, offset: 0, height: 50 }
+        const config = { steps: 200, opacity: 1, waveLength: 50, speed: 250, offset: 0, height: 50, widthReductor: 3 }
         ctx.imageSmoothingEnabled = true;
-        const configs = [ {...config, opacity: 1}, {...config, opacity: 0.5, offset: 2, speed: 350, waveLength: 75, height: 40}, {...config, opacity: 0.5, offset: 3, speed: 450, waveLength: 40, wavlength: 75 } ]
+        const configs = [ {...config, opacity: 1}, {...config, opacity: 0.5, offset: 2, speed: 350, waveLength: 75, height: 40}, {...config, opacity: 0.5, offset: 3, speed: 450, waveLength: 40 } ]
         this.time.on('tick', () => {
             ctx.clearRect(0, 0, width, height)
             this.drawWave(ctx, width, height, configs[0])
@@ -101,6 +101,10 @@ export default class WorldBuilder extends Component {
         ctx.translate(0.5, height/2)
         ctx.translate(0.5, 0.5);
         ctx.beginPath()
+        ctx.shadowOffsetX = 0
+        ctx.shadowOffsetY = 0
+        ctx.shadowBlur = 3
+        ctx.shadowColor = 'white'
         ctx.strokeStyle = `rgba(255, 255, 255, ${config.opacity})`
         const inflexionPoint = (this.rangeValue.value) / WORLDBUILDER_MAX_VALUE
         // const sineLimits = [ Math.max(inflexionPoint - 0.5, 0), Math.min(inflexionPoint + 0.5, 1) ]
@@ -110,7 +114,7 @@ export default class WorldBuilder extends Component {
             // debugger;
             let y = 0
             if (inRange(x/config.steps, sineLimits[0], sineLimits[1]))
-                y = Math.sin((x - sineLimits[0]) * 1/(sineLimits[1] - sineLimits[0]) * ((Math.PI)/2) / config.waveLength + this.time.elapsed/config.speed + config.offset) * Math.sin(this.time.elapsed/config.speed) * Math.max((1 - inflexionPointDistance * 3), 0)
+                y = Math.sin((x - sineLimits[0]) * 1/(sineLimits[1] - sineLimits[0]) * ((Math.PI)/2) / config.waveLength + this.time.elapsed/config.speed + config.offset) * Math.sin(this.time.elapsed/config.speed) * Math.max((1 - inflexionPointDistance * config.widthReductor), 0)
             ctx.lineTo( x/config.steps * width, y * config.height )
         }
         ctx.stroke()
