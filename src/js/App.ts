8f046199b51b7b0b89f9d59ffe5592caf661bf0e
romanechouteau <romane.chouteau@gmail.com>
2021-06-28 10:40:55 +0200

    🐛 remove fps stats and loader

--- src/js/App.ts
+++ src/js/App.ts
@@ -34,7 +34,6 @@ import {
 
 const stats = new Stats()
 stats.showPanel(0) // 0: fps, 1: ms, 2: mb, 3+: custom
-document.body.appendChild(stats.dom)
 
 export default class App extends Component {
   canvas: any
@@ -133,6 +132,7 @@ export default class App extends Component {
       folder
         .add(this.renderOnBlur, 'activated')
         .name('Render on window blur')
+      document.body.appendChild(stats.dom)
     }
   }
   setCamera() {
